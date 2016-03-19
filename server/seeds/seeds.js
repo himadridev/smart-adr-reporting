4493164c3250f6007337af73253247e5a84feb7b
Jagadish, K <k.jagdish@gmail.com>
2016-03-20 01:21:21 +0530

    Uniq id and meta on medicines

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -12,18 +12,31 @@ Meteor.startup(function () {
         ProductNo: "001",
         drugName: "Esmolol Hydrochloride Injection",
         manufacturerName: "Baxter Healthcare S.A.",
-        manufacturerAddress: "Moneen Road, Castlebar, County Mayo , Ireland (IRL)"
+        manufacturerAddress: "Moneen Road, Castlebar, County Mayo , Ireland (IRL)",
+        feedback: {
+          "positive" : 0,
+          "negative" : 0
+        },
+        keywords: ['esmolol'],
+        hasNotification : false
       },
       {
         ApplNo: "087082",
         ProductNo: "001",
         drugName: "Tylenol",
         manufacturerName: "Jhonson and Jhonson",
-        manufacturerAddress: "New Brunswick, New Jersey"
+        manufacturerAddress: "New Brunswick, New Jersey",
+        feedback: {
+          "positive" : 0,
+          "negative" : 0
+        },
+        keywords: ['tylenol','acetaminophen'],
+        hasNotification : false
       }
     ];
 
     for(var i = 0; i < products.length; i++){
+      products[i].shortid = ShortId.generate();
       Medicines.insert(products[i]);
     }
   }
