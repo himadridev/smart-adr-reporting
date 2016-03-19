a705b7341659bcfe01cfe44cef9b13714390c260
Jagadish, K <k.jagdish@gmail.com>
2016-03-19 14:22:50 +0530

    Function and Methods for fetching tweets and getting sentiment

--- server/functions/functions.js
+++ server/functions/functions.js
@@ -1,3 +1,33 @@
-/**
- * Created by himadri on 3/19/16.
- */
+Fiber = Npm.require('fibers');
+
+Utils = {
+  
+  storeTweetIfUnique: function(err, data, response) {
+    var i, numTweets, tmp, tweetId, tweetCount, resp;
+    var statuses = data.statuses;
+    numTweets = statuses.length;
+    for (i = 0; i < numTweets; i++) {
+      console.log(statuses[i]);
+      tweetId = statuses[i].id_str;
+      tweetCount = TweetSentiment.find({
+          txtId: tweetId
+        }).count() ;
+      
+
+      if ( !tweetCount ) {
+        tmp = {};
+        tmp.txtId = tweetId;
+        tmp.text = statuses[i].text;
+
+        // tmp.sentiment = Meteor.call('fetchTextSentiment', statuses[i].text);
+        
+        TweetSentiment.insert(tmp);
+
+        // if(tmp.sentiment){
+
+        // }
+        
+      }
+    }
+  }
+}
--- server/methods/methods.js
+++ server/methods/methods.js
@@ -1,3 +1,34 @@
-/**
- * Created by himadri on 3/19/16.
- */
+Meteor.methods({
+  fetchTweetsFromTwitter: function(keyword) {
+    var T = new Twit({
+      consumer_key: Meteor.settings.twitterConsumerKey,
+      consumer_secret: Meteor.settings.twitterConsumerSecret,
+      access_token: Meteor.settings.twitterAccessToken,
+      access_token_secret: Meteor.settings.twitterAccessTokenSecret
+    });
+
+    T.get('search/tweets', {
+      q: keyword,
+      count: 5
+    }, Meteor.bindEnvironment(Utils.storeTweetIfUnique));
+  },
+
+  fetchTextSentiment: function(txt) {
+    var alchemyURL = Meteor.settings.alchemyUrl;
+    var postData = {
+      'apikey': '560e4a4c8b48b771bcedfd2d0e8718557aed0b46',
+      'text': txt,
+      'outputMode': 'json'
+    };
+    var options = {
+      params: postData,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+    var sentimentData = HTTP.call('POST', alchemyURL, options)
+      
+    
+    return sentimentData.data.docSentiment.type;
+  }
+})
