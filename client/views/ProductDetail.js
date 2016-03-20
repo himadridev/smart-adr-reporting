22ee9be0fe608075c2778a649e47b1d6dc8db542
Jagadish, K <k.jagdish@gmail.com>
2016-03-20 09:17:43 +0530

    Initial detail page

--- client/views/ProductDetail.js
+++ client/views/ProductDetail.js
@@ -1,10 +1,22 @@
 Template.ProductDetail.created = function () {
   var tmpl = this;
   tmpl.productData = new ReactiveDict();
-
-  Meteor.call('fetchProductDetail', function(resp){
-    if(resp){
-      tmpl.productData = resp;
-    }
-  });
+  this.autorun(function(){
+    var prodid = Router.current().params.shortid;
+    tmpl.productData.set('data', Medicines.findOne({shortid : prodid}))  ;
+  })
 };
+
+Template.ProductDetail.helpers({
+  prodname: function () {
+    return Template.instance().productData.get('data') ? Template.instance().productData.get('data').drugName : 'Fetching...';
+  },
+
+  manufacturer: function(){
+    return Template.instance().productData.get('data') ? Template.instance().productData.get('data').manufacturerName : '...';
+  },
+
+  manAddress: function(){
+    return Template.instance().productData.get('data') ? Template.instance().productData.get('data').manufacturerAddress : '...';
+  }
+});
